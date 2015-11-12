17e0d80682324af419d42ba6b4b7bc01b891880b
Bastrykov Evgeniy <vporoshok@gmail.com>
2015-11-12 18:01:02 +0400

    add salt

--- check_password.py
+++ check_password.py
@@ -3,16 +3,25 @@
 
 import hashlib
 
-username = input('Enter username: ').encode('utf-8')
-password = input('Enter password: ').encode('utf-8')
-
-hashed_password = hashlib.md5(password).hexdigest()
+username = input('Enter username: ')
+password = input('Enter password: ')
 
 with open('passwords.txt') as f:
     for line in f.readlines():
-        if line == '{0} {1}\n'.format(username, hashed_password):
-            print('ok')
-            break
+        line = line.strip('\n')
+        (login, hashed_password_with_salt) = line.split(' ')
+        if login == username:
+            (salt, hashed_password) = hashed_password_with_salt.split('$')
+            entered_hashed_password = hashlib.md5()
+            entered_hashed_password.update((password + salt).encode())
+            entered_hashed_password = entered_hashed_password.hexdigest()
+            if hashed_password == entered_hashed_password:
+                print('ok')
+                break
+
+            else:
+                print('error')
+                break
 
     else:
         print('error')
--- new_password.py
+++ new_password.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
+import random
 
-username = input('Enter username: ').encode('utf-8')
-password = input('Enter password: ').encode('utf-8')
+ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-hashed_password = hashlib.md5(password).hexdigest()
+def gen_salt(length=8):
+
+    return ''.join(random.choice(ALPHABET) for i in range(length))
+
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+salt = gen_salt()
+
+hashed_password = hashlib.md5('{0}{1}'.format(password, salt).encode()).hexdigest()
 
 with open('passwords.txt', 'a') as f:
-    f.write('{0} {1}\n'.format(username, hashed_password))
+    f.write('{0} {1}${2}\n'.format(username, salt, hashed_password))
