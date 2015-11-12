129446da099460e3375564fd91a356272bd283ec
Bastrykov Evgeniy <vporoshok@gmail.com>
2015-11-12 17:23:28 +0400

    plain text

---
+++ check_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+with open('passwords.txt') as f:
+    for line in f.readlines():
+        if line == '{0} {1}'.format(username, password):
+            print('ok')
+            break
+
+    else:
+        print('error')
---
+++ new_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+with open('passwords.txt', 'a') as f:
+    f.write('{0} {1}'.format(username, password))
